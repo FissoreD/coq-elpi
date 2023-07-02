f77d984387a514a89a0b2ce0dcc0f62f0e377b67
Davide Fissore <fisso9900@gmail.com>
2023-07-02 15:05:32 +0200

    WIP

--- tests/elpi2html.v
+++ tests/elpi2html.v
@@ -1,4 +1,11 @@
 From elpi Require Import elpi.
+(* From elpi Extra Dependency "elpi2html2.elpi" as elpi2html. *)
+(* 
+Elpi Command C. 
+Elpi Accumulate File elpi2html.
+Elpi Typecheck. *)
+
+
 Elpi Command Test.
 Elpi Accumulate lp:{{
   main _ :- coq.say "CIAO".
